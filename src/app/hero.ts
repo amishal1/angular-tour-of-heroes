91fae63835802eebdf1824bae4012a21102cfdc5
amishal1 <humanabstract1.1@gmail.com>
2018-03-31 18:16:42 +0500

    second commit

---
+++ src/app/hero.ts
@@ -0,0 +1,5 @@
+export class Hero {
+  id: number;
+  name: string;
+  photo: string; // to add images, a variable called photos added
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+//The list of Heroes used in the application and link for the photo variable was added.
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Iron Man', photo: "assets/images/ironman.jpg" },
+  { id: 12, name: 'Spider Man' , photo: "assets/images/spiderman.jpg" },
+  { id: 13, name: 'Captain America' , photo: "assets/images/capamerica.jpg" },
+  { id: 14, name: 'Thor' , photo: "assets/images/thor.jpg" },
+  { id: 15, name: 'Dr Strange' , photo: "assets/images/drstrange.jpg" },
+  { id: 16, name: 'Black Widow' , photo: "assets/images/blackwidow.jpg" },
+  { id: 17, name: 'Black Panther' , photo: "assets/images/blackpanther.jpeg" },
+  { id: 18, name: 'Hulk' , photo: "assets/images/hulk.jpg" },
+  { id: 19, name: 'Wolverine' , photo: "assets/images/wolverine.jpg" },
+  { id: 20, name: 'Storm' , photo: "assets/images/storm.jpg" }
+];
